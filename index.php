cae6f760076eff51bc73c1face10c952d9dd9246
Marie-Axelle Simoes <Vous@exemple.com>
2017-12-13 09:10:11 +0100

    boucle chiffre

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>php boucle</title>
+  </head>
+  <body>
+    <?php
+    $count= 0;
+
+    while ($count <= 10) {
+      echo $count;
+      $count++;
+    }
+     ?>
+  </body>
+</html>
